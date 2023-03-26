318065fa18e60b5c28fbbf13fb3054c617b42d3e
Rishabh Sharma <89383192+rishabhsharma5554@users.noreply.github.com>
2023-03-26 22:02:29 +0530

    RatingService Feign ClientCommunication

---
+++ UserMService/src/main/java/com/rishabhtech/userservice/external/services/RatingService.java
@@ -0,0 +1,26 @@
+package com.rishabhtech.userservice.external.services;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.rishabhtech.userservice.entity.Rating;
+
+@FeignClient
+public interface RatingService {
+
+	//get
+	
+	//post
+	@PostMapping("/ratings")
+	public Rating createRating(Rating values);
+	
+	//put
+	@PutMapping("/ratings/{ratId}")
+	public Rating updateRating(@PathVariable String ratId,Rating rating);
+
+	@DeleteMapping("/ratings/{ratId}")
+	public void deleteRating(@PathVariable String ratId);
+}
