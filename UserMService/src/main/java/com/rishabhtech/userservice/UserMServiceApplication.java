8872a4d94377714edd50634561b620aa277cdd69
Rishabh Sharma <89383192+rishabhsharma5554@users.noreply.github.com>
2023-03-26 21:41:01 +0530

    Revert "Feign Client Communication"
    This reverts commit 64d54ef49ce1f794c3b85689a247c37bf7450c28.

--- UserMService/src/main/java/com/rishabhtech/userservice/UserMServiceApplication.java
+++ UserMService/src/main/java/com/rishabhtech/userservice/UserMServiceApplication.java
@@ -2,10 +2,8 @@ package com.rishabhtech.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class UserMServiceApplication {
 
 	public static void main(String[] args) {
